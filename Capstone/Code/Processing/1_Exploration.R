e82367f888a9493db5e58365637449024a10d8ef
DannyGsGit <danny.godbout@gmail.com>
2016-11-06 14:09:35 -0800

    Added the following to 1_Exploration.R: * Data import * Separate features by type (int, factor) * Scale and plot integer features to inspect information content

--- Capstone/Code/Processing/1_Exploration.R
+++ Capstone/Code/Processing/1_Exploration.R
@@ -1,2 +1,28 @@
+library(ggplot2)
+library(reshape2)
 
 #### Load data for exploration ####
+my.data <- read.csv("./Data/Raw/train.csv")
+
+#### Explore variables ####
+
+# High-level data summary
+str(my.data)
+
+# Sort parameters by type
+data.types <- sapply(my.data, class)
+int.columns <- which(data.types == "integer")
+
+## Normalize and plot integer params to inspect information content
+# Calculate zscores
+int.data.zscore <- scale(my.data[, int.columns], center = TRUE, scale = TRUE)
+
+# Melt data
+int.data.zscore <- melt(int.data.zscore)
+
+# Plot
+int.boxplots <- ggplot(int.data.zscore, aes(Var2, value)) +
+  geom_boxplot() +
+  coord_flip()
+
+int.boxplots
